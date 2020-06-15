f53a8ded1ba28277427a6e32912fdaa13b56ab93
Leclere <leclere.corentin@gmail.com>
2020-06-15 14:39:23 +0200

    first commit

---
+++ initialiser_depot_git.bat
@@ -0,0 +1,8 @@
+echo "# autoformation_fullstack" >> README.md
+git init
+git add .
+git commit -m "first commit"
+git remote add origin https://github.com/ben5962/autoformation_fullstack.git
+git push -u origin master
+
+pause
